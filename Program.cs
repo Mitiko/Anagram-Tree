edef4367195d92257fb3d19c3d4aa110dccc531b
Mitiko <mitikodev@gmail.com>
2019-01-02 13:19:24 +0200

    Migrate to netcoreapp2.2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Words
+namespace Anagram_Tree
 {
     public class Program
     {
