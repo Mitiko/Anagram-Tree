c0714284810573f6e28e916791c0861bb688526c
Mitiko <mitikodev@gmail.com>
2019-01-02 13:55:25 +0200

    Add models and application context

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Anagram_Tree.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Word> Word { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;User Id=admin;Password=god;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Word>(entity =>
+            {
+                entity.ToTable("word");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .HasDefaultValueSql("NULL::character varying");
+            });
+        }
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anagram_Tree.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
