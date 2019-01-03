289bc6026ef8ee31863b16a113d5a68934eac72b
Mitiko <mitikodev@gmail.com>
2019-01-03 11:52:46 +0200

    Add application insights

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Anagram_Tree
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
